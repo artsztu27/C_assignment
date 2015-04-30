e9ecbfd4539224dea7ee9be5c9d2136c6b93c2ca
artsztu27 <artsztu27@gmail.com>
2015-04-30 10:26:12 +0800

    my first commit

---
+++ Artsz_assignment1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+
+            string firstName = "Mary";
+            string lastName = "Wang";
+            string fullName = firstName+" "+lastName;
+            string year = "1994";
+            string date = "23";
+            string Month = "December";
+            string Birthday = Month + " " + date + "," + year;
+            string Address1  ="2400 Cammino Ramon";
+            string Address2  ="Suite 375";
+            string City = "Sam Ramon";
+            string State= "Pingtung";
+            int Zip = 90076;
+            string country = "Taiwan";
+            string Professor = "Chunhung Lin";
+            string pertinent = "Compuer Science";
+            string U_degree = "Master of Computer Science";
+            string  P_name ="Computer Science ";
+            string  D_offered = "Master";
+            string  D_Head = "NSYSU CSE";
+            string  course = "JAVA";
+            Console.WriteLine("Student information");
+            Console.WriteLine("First Name: {0}", firstName);
+            Console.WriteLine("Last Name: {0}", lastName);
+            Console.WriteLine("Birthdate: on {0}", Birthday);
+            Console.WriteLine("Address Line 1: {0}" ,Address1);
+            Console.WriteLine("Address Line 2: {0}" ,Address2);
+            Console.WriteLine("City: {0}", City);
+            Console.WriteLine("State/Province:{0}",State);
+            Console.WriteLine("Zip/Postal: {0}",Zip);
+            Console.WriteLine("Country:{0}",country);
+            Console.WriteLine("Professor information: {0} with {1}", Professor, pertinent);
+            Console.WriteLine("university degree: {0}  ", U_degree);
+            Console.WriteLine("Program name {0}  ", P_name);
+            Console.WriteLine("Degrees offered: {0}  ", D_offered);
+            Console.WriteLine("Department Head: {0}  ", D_Head);
+            Console.WriteLine("course: {0}  ", course);
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ artsz_assignment2/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 1;
+            int clumns = 1;
+            while (clumns < 9)
+            {
+                //   Console.WriteLine("clumns =" + clumns);
+                //   Console.WriteLine("clumns % 2 =" + (clumns%2));
+                rows = 1;
+                if ((clumns % 2) == 1)
+                {
+                    while (rows < 9)
+                    {
+                        //Console.WriteLine("rows1 =" + rows);
+                        switch (rows)
+                        {
+                            case 1:
+                                Console.Write("X");
+                                break;
+                            case 2:
+                                Console.Write("O");
+                                break;
+                            case 3:
+                                Console.Write("X");
+                                break;
+                            case 4:
+                                Console.Write("O");
+                                break;
+                            case 5:
+                                Console.Write("X");
+                                break;
+                            case 6:
+                                Console.Write("O");
+                                break;
+                            case 7:
+                                Console.Write("X");
+                                break;
+                            case 8:
+                                Console.WriteLine("O");
+                                break;
+
+                            default:
+                                break;
+                        }
+                        rows++;
+                    } 
+                }
+                else
+                {
+                  //  Console.WriteLine("rows2 =" + rows);
+                    while (rows < 9)
+                    {
+                        switch (rows)
+                        {
+                            case 1:
+                                Console.Write("O");
+                                break;
+                            case 2:
+                                Console.Write("X");
+                                break;
+                            case 3:
+                                Console.Write("O");
+                                break;
+                            case 4:
+                                Console.Write("X");
+                                break;
+                            case 5:
+                                Console.Write("O");
+                                break;
+                            case 6:
+                                Console.Write("X");
+                                break;
+                            case 7:
+                                Console.Write("O");
+                                break;
+                            case 8:
+                                Console.WriteLine("X");
+                                break;
+
+                            default:
+                                break;
+                        }
+                        rows++;
+                    }
+                }
+             clumns++; 
+            }
+         
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();    
+        }
+    }
+}
